a749f44fb7499b4b693e5bae972af53b9b9d8df3
PupoSDC <pupo163@hotmail.com>
2024-12-07 16:22:45 +0100

    feat: solve problem 6.2

--- code/0602.ts
+++ code/0602.ts
@@ -1,69 +1,81 @@
-const data = await Deno.readTextFile("./data/0600.txt");
-const map = data.split("\n").map((s) => s.split(""));
+const data = await Deno.readTextFile("./data/0601.txt");
+const allMaps = data
+    .split("")
+    .map((l, i, arr) => {
+        const newArr = [...arr];
+        if (l === ".") newArr[i] = "O";
+        return newArr.join("");
+    })
+    .filter((l) => l.includes("O"))
+    .map((l) => l.replace("O", "#").split("\n").map((s) => s.split("")));
+    
 
-let x: number = 0;
-let y: number = 0;
-let loopCount: number = 0;
-let dir: "UP" | "LEFT" | "RIGHT" | "DOWN" = "UP";
+let loops = 0;
+for (let i = 0; i < allMaps.length; i++) {
+    console.log(`map ${i + 1} / ${allMaps.length}`);
 
-map.forEach((line, _y) => {
-  line.forEach((entry, _x) => {
-    if (entry === "^") {
-      x = _x;
-      y = _y;
-      dir === "UP";
+    const map = allMaps[i];
+    let iterations = 0;
+    let x: number = 0;
+    let y: number = 0;
+    let xCount: number = 0;
+    let dir: "UP" | "LEFT" | "RIGHT" | "DOWN" = "UP";
+    
+    map.forEach((line, _y) => {
+      line.forEach((entry, _x) => {
+        if (entry === "^") {
+          x = _x;
+          y = _y;
+          dir === "UP";
+        }
+      });
+    });
+    
+    while (
+      y > 0 && y < map.length &&
+      x > 0 && x < map[0].length &&
+      iterations < 1E6
+    ) {
+      if (map[y][x] !== "X") xCount++;
+      map[y][x] = "X";
+      iterations++;
+    
+      if (dir === "UP" && map[y - 1]?.[x] === "#") {
+        dir = "RIGHT";
+        continue;
+      }
+      if (dir === "RIGHT" && map[y]?.[x + 1] === "#") {
+        dir = "DOWN";
+        continue;
+      }
+      if (dir === "DOWN" && map[y + 1]?.[x] === "#") {
+        dir = "LEFT";
+        continue;
+      }
+      if (dir === "LEFT" && map[y]?.[x - 1] === "#") {
+        dir = "UP";
+        continue;
+      }
+      if (dir === "UP") {
+        y--;
+        continue;
+      }
+      if (dir === "RIGHT") {
+        x++;
+        continue;
+      }
+      if (dir === "DOWN") {
+        y++;
+        continue;
+      }
+      if (dir === "LEFT") {
+        x--;
+        continue;
+      }
     }
-  });
-});
-
-while (
-  y > 0 && y < map.length &&
-  x > 0 && x < map[0].length
-) {
-  map[y][x] = dir;
-
-  if (dir === "UP" && map[y - 1]?.[x] === "#") {
-    dir = "RIGHT";
-    continue;
-  }
-  if (dir === "RIGHT" && map[y]?.[x + 1] === "#") {
-    dir = "DOWN";
-    continue;
-  }
-  if (dir === "DOWN" && map[y + 1]?.[x] === "#") {
-    dir = "LEFT";
-    continue;
-  }
-  if (dir === "LEFT" && map[y]?.[x - 1] === "#") {
-    dir = "UP";
-    continue;
-  }
-
-  if (dir === "UP") {
-    if (map[y][x] === "RIGHT") loopCount++;
-    map[y][x] = "UP";
-    y--;
-    continue;
-  }
-  if (dir === "RIGHT") {
-    x++;
-    if (map[y][x] === "DOWN") loopCount++;
-    map[y][x] = "RIGHT";
-    continue;
-  }
-  if (dir === "DOWN") {
-    if (map[y][x] === "LEFT") loopCount++;
-    map[y][x] = "DOWN";
-    y++;
-    continue;
-  }
-  if (dir === "LEFT") {
-    if (map[y][x] === "UP") loopCount++;
-    map[y][x] = "LEFT";
-    x--;
-    continue;
-  }
+    
+    if (iterations === 1E6) loops++;
 }
 
-console.log(loopCount);
-// console.log(map.map(l => l.join("")).join("\n"));
+
+console.log(loops);
